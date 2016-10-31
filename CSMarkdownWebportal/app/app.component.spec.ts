9d6e7c13f818e34753fa88d26ab0ddefbe3804ca
Axelfarm <nichoaxel@hotmil.com>
2016-10-31 10:42:30 +0100

    Test

--- CSMarkdownWebportal/app/app.component.spec.ts
+++ CSMarkdownWebportal/app/app.component.spec.ts
@@ -2,6 +2,7 @@
 import { AppComponent } from './app.component';
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { By }           from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
